9963935378e8397b0a166dac824e15d8c3929478
Florian <floriangomis76@gmail.com>
2024-11-24 11:54:49 +0000

    add websocket Chat feature using DynamoDB as storage

--- src/handler.ts
+++ src/handler.ts
@@ -1,12 +1,20 @@
 import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
 import { S3 } from "@aws-sdk/client-s3";
+const { ApiGatewayManagementApiClient, PostToConnectionCommand } = require('@aws-sdk/client-apigatewaymanagementapi');
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { PutCommand, DeleteCommand, GetCommand, ScanCommand } = require("@aws-sdk/lib-dynamodb");
+import { v4 as uuidv4 } from 'uuid';
 
-// MongoDB and S3 setup
+// MongoDB setup
 import { MongoClient } from "mongodb";
-
 const mongodbUri = process.env.MONGODB_URI!;
+// S3 setup
 const s3 = new S3();
 const bucketName = process.env.S3_BUCKET_NAME!;
+// DynamoDB setup
+const client = new DynamoDBClient();
+const CONNECTIONS_TABLE_NAME = "WebSocketConnections";
+const MESSAGES_TABLE_NAME = "MessageLogs";
 
 let mongoClient: MongoClient | null = null;
 async function getMongoClient() {
@@ -142,6 +150,213 @@ export const uploadPicture = async (
     return {
       statusCode: 500,
       body: JSON.stringify({ error: "Failed to upload picture" }),
+    };
+  }
+};
+
+//  WEBSOCKET SECTION 
+
+// This can't work since the lambda are not sharing memory ....
+
+const apiGatewayClient = new ApiGatewayManagementApiClient({
+  endpoint: process.env.WEBSOCKET_ENDPOINT,
+});
+
+export const identify = async (event: APIGatewayEvent) => {
+  const { connectionId } = event.requestContext;
+  const { username } = JSON.parse(event.body || '{}');
+
+  const command = new GetCommand({
+    TableName: CONNECTIONS_TABLE_NAME,
+    Key: {
+      connectionId: connectionId,
+    },
+  });
+  const result = await client.send(command);
+  if (result.Item) {
+    try {
+      const updateCommand = new PutCommand({
+        TableName: CONNECTIONS_TABLE_NAME,
+        Item: {
+          connectionId: connectionId,
+          username: username,
+          timestamp: new Date().toISOString(),
+        },
+      });
+      await client.send(updateCommand);
+
+    // Fetch the latest logs from Dynamo
+    const latestLogsCmd = new ScanCommand({
+      TableName: MESSAGES_TABLE_NAME,
+    });
+    const latestLogs = (await client.send(latestLogsCmd)).Items;
+    // sort latestlogs by Date and time using the timestamp property
+    const sortedLatestLogs: any[]= latestLogs.sort((a: any, b: any) => {
+      return new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime();
+    }).reverse();
+      const postToConnectionCommand = new PostToConnectionCommand({
+        ConnectionId: connectionId,
+        Data: JSON.stringify({
+          sortedLatestLogs
+        }),
+      });
+      await apiGatewayClient.send(postToConnectionCommand);
+      return { 
+        statusCode: 200,
+        body: 'Username identified.',
+        headers: {
+          "Access-Control-Allow-Origin": "*", // Allow requests from any origin
+          "Access-Control-Allow-Methods": "GET,POST,OPTIONS", // Allow specific HTTP methods
+          "Access-Control-Allow-Headers": "Content-Type,Authorization", // Allow specific headers
+        },
+      };
+    } catch (error) {
+      console.error("Error saving User : ", error);
+      return {
+        statusCode: 500,
+        body: "Failed to identify username.",
+      };
+    }
+  } 
+}
+
+export const connect = async (event: APIGatewayEvent) => {
+  const { connectionId } = event.requestContext;
+  console.log(`ConnectionId: ${connectionId}`);
+  if ( connectionId ) {
+    try {
+      const command = new PutCommand({
+        TableName: CONNECTIONS_TABLE_NAME,
+        Item: {
+          connectionId: connectionId,
+          timestamp: new Date().toISOString(),
+        },
+      });
+  
+      await client.send(command);
+      return { 
+        statusCode: 200,
+        body: `${connectionId} Connected.`,
+        headers: {
+          "Access-Control-Allow-Origin": "*", // Allow requests from any origin
+          "Access-Control-Allow-Methods": "GET,POST,OPTIONS", // Allow specific HTTP methods
+          "Access-Control-Allow-Headers": "Content-Type,Authorization", // Allow specific headers
+        },
+      };
+    } catch (error) {
+      console.error("Error saving User : ", error);
+      return {
+        statusCode: 500,
+        body: "Failed to connect.",
+      };
+    }
+  }
+};
+
+export const disconnect = async (event: APIGatewayEvent) => {
+  const { connectionId } = event.requestContext;
+  try {
+    const command = new DeleteCommand({
+      TableName: CONNECTIONS_TABLE_NAME,
+      Key: {
+        connectionId: connectionId,
+      },
+    });
+    await client.send(command);
+    return { 
+      statusCode: 200,
+      body: `${connectionId} Disconnected.`,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Allow requests from any origin
+        "Access-Control-Allow-Methods": "GET,POST,OPTIONS", // Allow specific HTTP methods
+        "Access-Control-Allow-Headers": "Content-Type,Authorization", // Allow specific headers
+      },
+    };
+  } catch (error) {
+    console.error("Error removing connection:", error);
+    return {
+      statusCode: 500,
+      body: "Failed to disconnect.",
+    };
+  }
+  
+};
+
+export const sendMessage = async (event: APIGatewayEvent) => {
+  const { message } = JSON.parse(event.body || '{}');
+  const { connectionId } = event.requestContext;
+
+  try {
+    // Fetch the user from DynamoDB
+    const getCommand = new GetCommand({
+      TableName: CONNECTIONS_TABLE_NAME,
+      Key: {
+        connectionId: connectionId,
+      },
+    });
+    const macthedUser = await client.send(getCommand);
+
+    if (!macthedUser.Item) {
+      console.error("Connection not found:", connectionId);
+      return {
+        statusCode: 404,
+        body: "Connection not found.",
+      };
+    }
+
+    const username = macthedUser.Item.username || "Anonymous";
+    console.log(`Message from ${username}: ${message}`);
+
+    const messageLogCommand = new PutCommand({
+      TableName: MESSAGES_TABLE_NAME,
+      Item: {
+        id: uuidv4(),
+        connectionId: connectionId,
+        username: username,
+        message: message,
+        timestamp: new Date().toISOString(),
+      },
+    });
+    await client.send(messageLogCommand);
+    
+    const latestLogsCmd = new ScanCommand({
+      TableName: MESSAGES_TABLE_NAME,
+    });
+    const latestLogs = (await client.send(latestLogsCmd)).Items;
+
+    // sort latestlogs by Date and time using the timestamp property
+    const sortedLatestLogs: any[]= latestLogs.sort((a: any, b: any) => {
+      return new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime();
+    }).reverse();
+
+    // Send the message to all connected clients
+    const allUsersCommand = new ScanCommand({
+      TableName: CONNECTIONS_TABLE_NAME,
+    });
+    const allUsers = await client.send(allUsersCommand);
+    const allConnections = allUsers.Items.map((item: any) => item.connectionId);
+    for (const connectionId of allConnections) {
+      const postToConnectionCommand = new PostToConnectionCommand({
+        ConnectionId: connectionId,
+        Data: JSON.stringify({
+          sortedLatestLogs
+        }),
+      });
+      await apiGatewayClient.send(postToConnectionCommand);
+    }
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Allow requests from any origin
+        "Access-Control-Allow-Methods": "GET,POST,OPTIONS", // Allow specific HTTP methods
+        "Access-Control-Allow-Headers": "Content-Type,Authorization", // Allow specific headers
+      },
+    };
+  } catch (error) {
+    console.error("Error handling action:", error);
+    return {
+      statusCode: 500,
+      body: "Failed to handle message.",
     };
   }
 };
